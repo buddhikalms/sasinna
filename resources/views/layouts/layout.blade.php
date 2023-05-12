bf8f07b657f549d8839845724000e3a0524b3005
buddhikalms <buddhikalms2002.com>
2023-05-12 13:52:59 +0530

    Add existing file

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Buddhika Viraj | Laravel Simple User Crud</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container" style="margin-top: 15px;">
+    @yield('content')
+</div>
+   
+</body>
+</html>
